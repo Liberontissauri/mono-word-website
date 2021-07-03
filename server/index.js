32da1932372767bc7e5df2b423b1bfaaa63e2cf0
Liberontissauri <jp.liberon@gmail.com>
2021-07-03 13:29:00 +0100

    base folder structure

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const path = require("path")
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public/build')))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('*', function(req, res) {
+  res.sendFile('index.html', {root: path.join(__dirname, 'public/build')});
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
