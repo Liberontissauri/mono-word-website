e030af24047d3494954d230b1dac49bd67d68bda
Liberontissauri <jp.liberon@gmail.com>
2021-07-06 16:22:02 +0100

    added onclick prop to button

--- client/src/components/MonoButton/MonoButton.js
+++ client/src/components/MonoButton/MonoButton.js
@@ -4,7 +4,7 @@ import "./MonoButton.css"
 function MonoButton(props) {
     return (
         <div>
-            <button className="mono-button text">{props.content}</button>
+            <button className="mono-button text" onClick={props.onClick}>{props.content}</button>
         </div>
     )
 }
