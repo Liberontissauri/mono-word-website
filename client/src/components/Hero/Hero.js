fb5cc054bfffe3ed9711b31c47aef17ec2b6bd33
Liberontissauri <jp.liberon@gmail.com>
2021-07-07 12:23:24 +0100

    adjusted hero position on page

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -2,11 +2,14 @@ import React from 'react'
 import icon from "../../img/logo.png"
 import "./Hero.css"
 
+import MonoButton from '../MonoButton/MonoButton'
+
 function Hero() {
     return (
         <div className="bg-div">
             <img src={icon} className="hero-icon"></img>
             <h2 className="text hero-h2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae eget consectetur pellentesque varius mus id. Sed urna faucibus eget suspendisse faucibus. consectetur adipiscing elit.</h2>
+            <MonoButton content="Invite Now"></MonoButton>
         </div>
     )
 }
